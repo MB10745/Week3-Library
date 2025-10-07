5db6c1fcd51c894526fc141a783c30a1304f9534
Mahnoor Baig <Baig-M4@ulster.ac.uk>
2025-10-07 10:43:59 +0300

    Added a constructor to the Book class.

--- Library1/Program.cs
+++ Library1/Program.cs
@@ -4,6 +4,14 @@
     string Author; //Second attribute
     string ISBN; //Third attribute
 
+    //Example of a constructor that allows us to 'construct' a new Book object
+    public Book(string bookTitle, string bookAuthor, string bookISBN)
+    {
+        Title = bookTitle;
+        Author = bookAuthor;
+        ISBN = bookISBN;
+    }
+
     //Declaring a method
     void Displayinfo()
     {
@@ -13,7 +21,11 @@
 
     static void Main(string[] args)
     {
-        //Create new instance (object) of the Book class
+        //Create new instance (object) of the Book class (with new constructor)
+        //Note how object name differs from class name
+        Book book = new Book("C# for beginners", "BillGates", "12345678");
+
+        /*//Create new instance (object) of the Book class
         //Note how object name differs from class name
         Book book = new Book();
 
@@ -32,10 +44,10 @@
         Book book1 = new Book();
         book1.Title = "C# Methods and classes";
         book1.Author = "Microsoft";
-        book1.ISBN = "55667778";
+        book1.ISBN = "55667778";*/
 
-        //Output second book info to the console (using method)
-        book1.Displayinfo();
+        //Output book info to the console (using method)
+        //book1.Displayinfo();
         book.Displayinfo();
     }
 }
