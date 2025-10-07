677ea180bdbbc51b1e3a7cec38b1d54974dbd21a
Mahnoor Baig <Baig-M4@ulster.ac.uk>
2025-10-07 09:58:04 +0300

    Add project files.

---
+++ Library1/Program.cs
@@ -0,0 +1,23 @@
+﻿class Book
+{
+    string Title; //First attribute
+    string Author; //Second attribute
+    string ISBN; //Third attribute
+
+    static void Main(string[] args)
+    {
+        //Create new instance (object) of the Book class
+        //Note how object name differs from class name
+        Book book = new Book();
+
+        //This info is for one book in our library
+        book.Title = "C# for beginners";
+        book.Author = "BillGates";
+        book.ISBN = "12345678";
+
+        //Output the book info to the console
+        Console.WriteLine($"Book Title: {book.Title}");
+        Console.WriteLine($"Book Author: {book.Author}");
+        Console.WriteLine($"Book ISBN: {book.ISBN}");
+    }
+}
